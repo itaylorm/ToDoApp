4a1f5a2dbb589b68fbb83ea6f8b9379a3f59658e
Taylor Maxwell <taylor@imaxwell.net>
2024-01-04 08:59:26 -0800

    Removed unneed using in controller

--- TodoApi/Controllers/TodosController.cs
+++ TodoApi/Controllers/TodosController.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
-using System.Threading.Tasks;
 using TodoLibrary.Data;
-using TodoLibrary.DataAccess;
 using TodoLibrary.Models;
 
 namespace TodoApi.Controllers;
